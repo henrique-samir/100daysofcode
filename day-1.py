7faa88c5f135ff3e58cc51dca8dee73180b8b599
Henrique Samir <henriquesamir@outlook.com.br>
2024-03-18 22:40:49 -0400

    feat: print statement

---
+++ day-1.py
@@ -0,0 +1,26 @@
+# print("Hello Replit")
+
+# print("Well we")
+# print("just use more lines")
+# print("of code")
+
+# print("""Anything that starts
+# with three quotes, and ends
+# in three quotes can span
+# many lines and even contain " symbols
+# within it without freaking anything out!""")
+
+# print("= MUSIC + =")
+# print("> Songs")
+# print("> Albums")
+# print("> Artists")
+
+print("My name is Henrique")
+print("Today is March 18th, 2024")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+
+print("I am feeling motivated")
+
+print("You can follow my progress at replit.com/@henriquesamir")
