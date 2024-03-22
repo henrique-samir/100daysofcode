18be95412c4e6e4312f8b9f1adb36f07718ff1fd
Henrique Samir <henriquesamir@outlook.com.br>
2024-03-21 23:11:34 -0400

    feat: add day 4 challenge

---
+++ day-4.py
@@ -0,0 +1,20 @@
+your_name = input("What is your name? ")
+enemy_name = input("What is your worst enemy’s name? ")
+superpower = input("What is your superpower? ")
+location = input("Where do you live? ")
+food = input("What is your favorite food? ")
+
+print(
+    "Hello",
+    your_name,
+    "! Your ability to",
+    superpower,
+    "will make sure you never have to look at",
+    enemy_name,
+    "again. Go eat",
+    food,
+    "as you walk down the streets of",
+    location,
+    "and use",
+    superpower,
+)
