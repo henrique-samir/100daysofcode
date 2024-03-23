450f6de37eb7f62cd0ffa862f0cbbb472a9fd339
Henrique Samir <henriquesamir@outlook.com.br>
2024-03-22 20:11:51 -0400

    feat: add if/else statement

---
+++ day-5.py
@@ -0,0 +1,20 @@
+# myName = input("What's your name?: ")
+# if myName == "David":
+#  print("Welcome Dude!")
+#  print("You're just the baldest dude I've ever seen")
+# else:
+#  print("Who on earth are you?!")
+
+# drink = input("Do you prefer coffee or tea?")
+# if drink == "coffee":
+#   print("Tea is better.")
+# else:
+#   print("Excellent choice.")
+
+print("Marvel Movie Character Creator")
+print("--")
+what_character = input("Do you like 'hanging around'?: ")
+if what_character == "No":
+    print("Then you're not Spider-man")
+else:
+    print("You're Spider-man")
